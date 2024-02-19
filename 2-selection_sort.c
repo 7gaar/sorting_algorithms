d6288f8f95545521b6a28c7e0ee1664c0a45da53
7gaar <m3gamind10@gmail.com>
2024-02-19 14:36:45 +0300

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * selection_sort - a function that sorts an array using selection sort.
+ * @array: array to be sorted.
+ * @size: size of array.
+*/
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_index;
+	int temp;
+
+	if (size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
+		if (min_index != i)
+		{
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
+			print_array(array, size);
+		}
+	}
+}
